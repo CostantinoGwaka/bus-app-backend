0edf902878883fd7c177dc253c204363b4a23108
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-04-18 11:16:30 +0300

    init

--- src/main/java/com/example/busreservation/services/CustomUserDetailsService.java
+++ src/main/java/com/example/busreservation/services/CustomUserDetailsService.java
@@ -2,10 +2,28 @@ package com.example.busreservation.services;
 
 import com.example.busreservation.entities.AppUser;
 import com.example.busreservation.repository.AppUserRepository;
-import org.apache.catalina.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final AppUser appUser= appUserRepository.findByUserName(username).orElseThrow(() ->
+                new UsernameNotFoundException("User does not exist"));
+        return User.builder().username(appUser.getUserName())
+                .password(appUser.getPassword())
+                .roles(appUser.getRole()).
+                build();
+    }
+
+
 }
